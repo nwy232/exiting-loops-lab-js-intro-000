b7cce44cac0e16b93544eb23e2169d08e4bc22a5
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-28 18:44:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ function keepGoing(array, changeValue, skipValue){
 }
 
 function findBy(array, findFn){
-
+  let x=null
   for(var i=0;i<array.length;i++)
   {
     if(array[i]==findFn())
-      return array[i];
+      x=array[i];
   }
 
-  return null;
+  return x;
 }
