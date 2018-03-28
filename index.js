c68a5b32ef78c5db000ee5ed1cf900f244a4ee22
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-28 18:34:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+breakOut(array, changeValue, stopValue){
+  for(var i=0; i<stopValue;i++)
+  {
+    array[i]=changeValue;
+  }
+  return array;
+}
+
+keepGoing(array, changeValue, skipValue){
+  for(var i=0; i<stopValue;i++)
+  {
+    if(array[i]==skipValue)
+      continue;
+
+    else 
+      array[i]=changeValue;
+  }
+  return array;
+}
+
+findBy(array, findFn){
+  for(var i=0;i<array.length;i++)
+  {
+    if(array[i]=='findFn()')
+      break;
+  }
+
+  return null;
+}
